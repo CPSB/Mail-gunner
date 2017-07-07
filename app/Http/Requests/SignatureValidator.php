2d799efbcfee27baa6cf29e6f89941bdb432e5e7
Tjoosten <Tjoosten3@gmail.com>
2017-07-07 08:02:18 +0200

    Implement signature validator

--- app/Http/Requests/SignatureValidator.php
+++ app/Http/Requests/SignatureValidator.php
@@ -13,7 +13,7 @@ class SignatureValidator extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,11 @@ class SignatureValidator extends FormRequest
     public function rules()
     {
         return [
-            //
+            'city'          => 'required',
+            'postal_code'   => 'required',
+            'email'         => 'required',
+            'name'          => 'required',
+            'country_id'    => 'required'
         ];
     }
 }
